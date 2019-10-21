ed1e83f17ecb7d53e68dd87815f721da05c53fa4
Administrator <Administrator@DNC-01712111555>
2019-10-21 10:15:51 +0800

    Chapter01

---
+++ src/chapter01/FootBallPlayer.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+public class FootBallPlayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//程序运行的代码写在main里
+		System.out.println("武磊是一个好球员");
+		System.out.println("李皮是个好教练");
+		System.out.println("中国球迷心很累，人人都是数学家");
+		
+		System.out.println("***********东软跨境交易平台******************");
+		System.out.println("*					*");
+		System.out.println("*	1.制造商资料管理			*");
+		System.out.println("*	2.销售商品品牌管理			*");
+		System.out.println("*	3.商品详细信息管理			*");
+		System.out.println("*	4.商品类别管理			*");
+		System.out.println("*					*");
+		System.out.println("*****************************************");
+	}
+
+}
---
+++ src/chapter01/student.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("认真背单词");
+		System.out.println("好好敲代码");
+		System.out.println("努力找到好工作");
+
+	}
+
+}
